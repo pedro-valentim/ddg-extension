d060c7c93eb44861f1b900e609cef0a3588fd6a1
Pedro Valentim <pedro@znc.com.br>
2017-11-17 09:09:05 -0200

    first commit

--- js/background.js
+++
@@ -1,9 +0,0 @@
-
-// Recebe messages de content_scripts
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-    // lê `newIconPath` do request e lê `tab.id` de sender
-    chrome.browserAction.setIcon({
-        path: request.newIconPath,
-        tabId: sender.tab.id
-    });
-});
